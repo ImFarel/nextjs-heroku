144a4aa14c040a913c02d1e7e51b732c96468b5d
ImFarel <sayafarel@gmail.com>
2020-04-19 06:59:34 +0700

    fix 2

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
+import Header from "../components/Header";
+import Layout from "../components/MyLayout";
+
 export default function About() {
-    return (
-      <div>
-        <p>This is the about page</p>
-      </div>
-    );
-  }
-  
+  return (
+    <Layout>
+      <p>This is the about page</p>
+    </Layout>
+  );
+}
